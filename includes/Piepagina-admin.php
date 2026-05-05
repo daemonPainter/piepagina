64dbc45f2fa59a9e2413522101f0ad87779d78cb
daemonPainter <mail@gabrieleomodeo.it>
2026-05-06 00:31:33 +0200

    adding admin panel base boilerplate

---
+++ includes/Piepagina-admin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The admin-specific functionality of the plugin.
+ */
+class Piepagina_Admin
+{
+    private $plugin_name;
+    private $version;
+
+    public function __construct($plugin_name, $version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
+
+    public function enqueue_styles()
+    {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . '../assets/css/admin.css', array(), $this->version, 'all');
+    }
+
+    public function enqueue_scripts()
+    {
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . '../assets/js/admin.js', array('jquery'), $this->version, false);
+    }
+}
